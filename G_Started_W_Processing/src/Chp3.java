5e9f5b1b53c544b40d98c6bb6305ced58bfa88fb
Freshield <zxdsw199182@gmail.com>
2016-05-16 19:23:23 -0400

    test normal figure

--- G_Started_W_Processing/src/Chp3.java
+++ G_Started_W_Processing/src/Chp3.java
@@ -21,7 +21,17 @@ public class Chp3 extends PApplet {
     }
 
     public void draw(){
-        point(240,60);
+//        quad(158,55,199,14,392,66,351,107);
+//        triangle(347,54,392,9,392,66);
+//        triangle(158,55,290,91,290,112);
+//        rect(180,60,220,40);
+//        ellipse(278,-100,400,400);
+//        ellipse(120,100,110,110);
+//        ellipse(412,60,18,18);
+        arc(90,60,80,80,0,radians(90));
+        arc(190,60,80,80,0,PI+HALF_PI);
+        arc(290,60,80,80,PI,TWO_PI+HALF_PI);
+        arc(390,60,80,80,QUARTER_PI,PI+QUARTER_PI);
     }
 
 }
