1bf0a6aeeabda9f204fc308e7f0f0b68b07dca39
Freshield <zxdsw199182@gmail.com>
2016-05-16 15:25:50 -0400

    begin Chp1

---
+++ G_Started_W_Processing/Chp1/src/Test1.java
@@ -0,0 +1,26 @@
+import processing.core.PApplet;
+
+/**
+ * Created by FRESHIELD on 2016/5/16.
+ */
+public class Test1 extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("Test1");
+    }
+
+    public void settings(){
+        super.settings();
+        size(800,800);
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+
+}
+
