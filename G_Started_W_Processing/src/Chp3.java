75726ad6139945a88d3dfb90b9c375137a3fe4fb
Freshield <zxdsw199182@gmail.com>
2016-05-16 19:36:13 -0400

    test pen weight

--- G_Started_W_Processing/src/Chp3.java
+++ G_Started_W_Processing/src/Chp3.java
@@ -12,11 +12,13 @@ public class Chp3 extends PApplet {
 
     public void settings(){
         super.settings();
-        size(480,120);
+        size(880,720);
+        smooth();
     }
 
     public void setup(){
 
+//        smooth();
 
     }
 
@@ -28,10 +30,26 @@ public class Chp3 extends PApplet {
 //        ellipse(278,-100,400,400);
 //        ellipse(120,100,110,110);
 //        ellipse(412,60,18,18);
-        arc(90,60,80,80,0,radians(90));
-        arc(190,60,80,80,0,PI+HALF_PI);
-        arc(290,60,80,80,PI,TWO_PI+HALF_PI);
-        arc(390,60,80,80,QUARTER_PI,PI+QUARTER_PI);
+//        arc(90,60,80,80,0,radians(90));
+//        arc(190,60,80,80,0,PI+HALF_PI);
+//        arc(290,60,80,80,PI,TWO_PI+HALF_PI);
+//        arc(390,60,80,80,QUARTER_PI,PI+QUARTER_PI);
+//        rect(160,30,260,20);
+//        ellipse(140,0,190,190);
+//
+//        strokeWeight(1);
+//        ellipse(75,60,90,90);
+//
+//        strokeWeight(8);
+//
+//        ellipse(175,60,90,90);
+//
+//        ellipse(275,60,90,90);
+//
+//        strokeWeight(20);
+//
+//        ellipse(400,60,90,90);
+        
     }
 
 }
