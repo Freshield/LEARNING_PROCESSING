5b2731fbd62110cb37be7d80f212a30a4c8aba71
Freshield <zxdsw199182@gmail.com>
2016-05-17 15:14:13 -0400

    chp6 begin

---
+++ G_Started_W_Processing/src/Chp6.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+/**
+ * Created by FRESHIELD on 2016/5/17.
+ */
+public class Chp6 extends PApplet {
+
+    public static void main(String[] args) {
+
+        PApplet.main("Chp5");
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+}
