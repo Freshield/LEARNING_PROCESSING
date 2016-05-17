f9e6b1bdde4b5adf806363a8e22edbef983da63f
Freshield <zxdsw199182@gmail.com>
2016-05-16 20:44:56 -0400

    chp4 begin

---
+++ G_Started_W_Processing/src/Chp4.java
@@ -0,0 +1,27 @@
+import processing.core.PApplet;
+
+/**
+ * Created by FRESHIELD on 2016/5/16.
+ */
+public class Chp4 extends PApplet {
+
+    public static void main(String[] args) {
+
+        PApplet.main("Chp3");
+    }
+
+    public void settings(){
+        super.settings();
+        size(720,480);
+
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+        
+    }
+
+}
