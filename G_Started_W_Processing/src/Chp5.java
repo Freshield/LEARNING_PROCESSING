99cf46f84e4ab6c26a771ae5eecfd7d78e100773
Freshield <zxdsw199182@gmail.com>
2016-05-17 15:03:56 -0400

    done chp5

--- G_Started_W_Processing/src/Chp5.java
+++ G_Started_W_Processing/src/Chp5.java
@@ -11,6 +11,14 @@ public class Chp5 extends PApplet{
         PApplet.main("Chp5");
     }
 
+    float x = 60;
+    float y = 440;
+    int radius = 45;
+    int bodyHeight = 160;
+    int neckHeight = 70;
+
+    float easing = 0.02f;
+
     public void settings(){
         super.settings();
         size(720,480);
@@ -21,14 +29,18 @@ public class Chp5 extends PApplet{
 //    int y = -100;
 //    int diameter = 380;
 
-    float x = 120;
-    int offset = 10;
-    float y = 60;
-    int radius = 12;
-    float px;
-    float py;
-    float easing = 0.05f;
-    float diameter = 12;
+//    float x = 215;
+//    int offset = 10;
+//    float y = 30;
+//    int radius = 12;
+//    int w = 80;
+//    int h = 60;
+//    float px;
+//    float py;
+//    float easing = 0.05f;
+//    float diameter = 12;
+
+
 
 
     public void setup(){
@@ -38,6 +50,10 @@ public class Chp5 extends PApplet{
 //        stroke(12);
 //        strokeWeight(30);
 //        x = width/2;
+//        ellipseMode(RADIUS);
+//        textSize(64);
+//        textAlign(CENTER);
+        strokeWeight(2);
         ellipseMode(RADIUS);
     }
 
@@ -107,17 +123,103 @@ public class Chp5 extends PApplet{
 //            line(x,0,x,height);
 //        }
 
-        background(204);
-        float d = dist(mouseX,mouseY,x,y);
-        if (d < radius) {
-            radius++;
-            fill(0);
+//        background(204);
+//        float d = dist(mouseX,mouseY,x,y);
+//        if (d < radius) {
+//            radius++;
+//            fill(0);
+//
+//        } else {
+//            fill(255);
+//        }
+//        ellipse(x,y,radius,radius);
 
+//        background(204);
+//        if ((mouseX > x) && (mouseX < x+w) && (mouseY > y) && (mouseY < y+h)) {
+//            fill(0);
+//        } else {
+//            fill(255);
+//        }
+//        rect(x,y,w,h);
+
+//        background(204);
+//        line(20,20,220,100);
+//        if (keyPressed) {
+//            line(220,20,20,100);
+//        }
+
+//        background(0);
+//        text(key,60,80);
+
+//        background(204);
+//        if (keyPressed) {
+//            if ((key == 'h') || (key == 'H')) {
+//                line(30,60,90,60);
+//            }
+//            if ((key == 'n') || (key == 'N')) {
+//                line(30,20,90,100);
+//            }
+//        }
+//        line(30,20,30,100);
+//        line(90,20,90,100);
+
+//        if (keyPressed && (key == CODED)) {
+//            if (keyCode == LEFT) {
+//                x--;
+//            } else if (keyCode == RIGHT) {
+//                x++;
+//            }
+//        }
+//        rect(x,45,50,50);
+
+
+        int targetX = mouseX;
+        x += (targetX -x)*easing;
+
+        if (mousePressed) {
+            neckHeight = 16;
+            bodyHeight = 90;
         } else {
-            fill(255);
+            neckHeight = 70;
+            bodyHeight = 160;
         }
-        ellipse(x,y,radius,radius);
+
+        float ny = y - bodyHeight - neckHeight - radius;
+
+        background(204);
+
+        //neck
+
+        stroke(102);
+        line(x+2,y-bodyHeight,x+2,ny);
+        line(x+12,y-bodyHeight,x+12,ny);
+        line(x+22,y-bodyHeight,x+22,ny);
+
+        //antenna
+        line(x+12,ny,x-18,ny-43);
+        line(x+12,ny,x+42,ny-99);
+        line(x+12,ny,x+78,ny+15);
 
 
+        //body
+        noStroke();
+        fill(102);
+        ellipse(x,y-33,33,33);
+        fill(0);
+        rect(x-45,y-bodyHeight,90,bodyHeight-33);
+        fill(102);
+        rect(x-45,y-bodyHeight+17,90,6);
+
+        //head
+        fill(0);
+        ellipse(x+12,ny,radius,radius);
+        fill(255);
+        ellipse(x+24,ny-6,14,14);
+        fill(0);
+        ellipse(x+24,ny-6,3,3);
+        fill(153);
+        ellipse(x,ny-8,5,5);
+        ellipse(x+30,ny-26,4,4);
+        ellipse(x+41,ny+6,3,3);
     }
 }
