5b9513dec8334d1f052555c1b3801b6473ec4dc9
Freshield <zxdsw199182@gmail.com>
2016-05-16 20:43:38 -0400

    done chp3

--- G_Started_W_Processing/src/Chp3.java
+++ G_Started_W_Processing/src/Chp3.java
@@ -12,7 +12,7 @@ public class Chp3 extends PApplet {
 
     public void settings(){
         super.settings();
-        size(880,720);
+        size(720,480);
 
     }
 
@@ -77,8 +77,8 @@ public class Chp3 extends PApplet {
 //        ellipse(228,-16,200,200);
 //        fill(0,0,255,160);
 //        ellipse(268,118,200,200);
-        fill(255);
-        beginShape();
+//        fill(255);
+//        beginShape();
 //        vertex(180,82);
 //        vertex(207,36);
 //        vertex(214,63);
@@ -87,19 +87,51 @@ public class Chp3 extends PApplet {
 //        vertex(219,82);
 //        vertex(226,109);
 //        vertex(180,82);
-        vertex(50,120);
-        vertex(100,90);
-        vertex(110,60);
-        vertex(80,20);
-        vertex(210,60);
-        vertex(160,80);
-        vertex(160,80);
-        vertex(200,90);
-        vertex(140,100);
-        vertex(130,120);
-        endShape();
+//        vertex(50,120);
+//        vertex(100,90);
+//        vertex(110,60);
+//        vertex(80,20);
+//        vertex(210,60);
+//        vertex(160,80);
+//        vertex(160,80);
+//        vertex(200,90);
+//        vertex(140,100);
+//        vertex(130,120);
+//        endShape();
+//        fill(0);
+//        ellipse(155,60,8,8);
+
+        strokeWeight(2);
+        background(204);
+        ellipseMode(RADIUS);
+
+        //neck
+
+        stroke(102);
+        line(266,257,266,162);
+        line(276,257,276,162);
+        line(286,257,286,162);
+
+        //body
+        noStroke();
+        fill(102);
+        ellipse(264,377,33,33);
         fill(0);
-        ellipse(155,60,8,8);
+        rect(219,257,90,120);
+        fill(102);
+        rect(219,274,90,6);
+
+        //head
+        fill(0);
+        ellipse(276,155,45,45);
+        fill(255);
+        ellipse(288,150,14,14);
+        fill(0);
+        ellipse(288,150,3,3);
+        fill(153);
+        ellipse(263,148,5,5);
+        ellipse(296,130,4,4);
+        ellipse(305,162,3,3);
 
     }
 
