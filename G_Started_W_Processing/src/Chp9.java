10ed46a12e2da044cf567319dead7650cb8d55b9
Freshield <zxdsw199182@gmail.com>
2016-05-18 13:00:42 -0400

    chp9 begin

---
+++ G_Started_W_Processing/src/Chp9.java
@@ -0,0 +1,26 @@
+import processing.core.PApplet;
+
+/**
+ * Created by FRESHIELD on 2016/5/18.
+ */
+public class Chp9 extends PApplet {
+
+    public static void main(String[] args) {
+
+        PApplet.main("Chp8");
+    }
+
+    public void settings(){
+        super.settings();
+        size(1400,1300);
+        smooth();
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+}
