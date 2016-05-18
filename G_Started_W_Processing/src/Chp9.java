c8fac946080d0c455436b9cfbce17a544aa2bc76
Freshield <zxdsw199182@gmail.com>
2016-05-18 14:49:02 -0400

    done chp9

--- G_Started_W_Processing/src/Chp9.java
+++ G_Started_W_Processing/src/Chp9.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.core.PShape;
 
 /**
  * Created by FRESHIELD on 2016/5/18.
@@ -12,6 +13,7 @@ public class Chp9 extends PApplet {
 
     Train red,blue;
     JitterBug bug,jit;
+    Robot bot1,bot2;
 
     public void settings(){
         super.settings();
@@ -26,15 +28,51 @@ public class Chp9 extends PApplet {
         blue = new Train("Blue Line",120);
         bug = new JitterBug(width*0.66f,height/2,20);
         jit = new JitterBug(width*0.33f,height/2,50);
+        bot1 = new Robot("robot1.svg",200,200);
+        bot2 = new Robot("robot2.svg",400,400);
 
     }
 
     public void draw(){
 
-        jit.move();
-        jit.display();
-        bug.move();
-        bug.display();
+//        jit.move();
+//        jit.display();
+//        bug.move();
+//        bug.display();
+
+        background(204);
+
+        bot1.update();
+        bot1.display();
+
+        bot2.update();
+        bot2.display();
+
+    }
+
+    public class Robot {
+
+        float xpos;
+        float ypos;
+        float angle;
+        PShape botShape;
+        float yoffset = 0.0f;
+
+        Robot (String svgName, float tempX, float tempY) {
+            botShape = loadShape(svgName);
+            xpos = tempX;
+            ypos = tempY;
+            angle = random(0,TWO_PI);
+        }
+
+        void update() {
+            angle += 0.05f;
+            yoffset = sin(angle) * 20;
+        }
+
+        void display() {
+            shape(botShape,xpos,ypos+yoffset);
+        }
 
     }
 
