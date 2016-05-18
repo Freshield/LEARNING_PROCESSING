8e0f0c226613901c3b5d4235b5d8c22b93d2000f
Freshield <zxdsw199182@gmail.com>
2016-05-18 14:50:12 -0400

    chp10 begin

---
+++ G_Started_W_Processing/src/Chp10.java
@@ -0,0 +1,31 @@
+import processing.core.PApplet;
+
+/**
+ * Created by FRESHIELD on 2016/5/18.
+ */
+public class Chp10 extends PApplet {
+
+
+    public static void main(String[] args) {
+
+        PApplet.main("Chp10");
+    }
+
+
+    public void settings() {
+        super.settings();
+        size(1400, 1300);
+        smooth();
+
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
+}
+
