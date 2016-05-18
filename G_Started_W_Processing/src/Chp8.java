e5807fba8c88422399c6cdd8efec6e19803315cf
Freshield <zxdsw199182@gmail.com>
2016-05-18 12:59:46 -0400

    done chp8

--- G_Started_W_Processing/src/Chp8.java
+++ G_Started_W_Processing/src/Chp8.java
@@ -29,15 +29,74 @@ public class Chp8 extends PApplet{
 
     public void draw(){
 
+//        background(204);
+//        randomSeed(0);
+//        for (int i = 35;i < width+40; i += 40) {
+//            int gray = (int)(random(0,102));
+//            float scalar = random(0.25f,1.0f);
+//            owl(i,100,gray,scalar);
+//        }
+
+//    float yourWeight = 132;
+//    float marsWeight = calculateMars(yourWeight);
+//    println(marsWeight);
+
         background(204);
-        randomSeed(0);
-        for (int i = 35;i < width+40; i += 40) {
-            int gray = (int)(random(0,102));
-            float scalar = random(0.25f,1.0f);
-            owl(i,100,gray,scalar);
-        }
+        drawRobot(120,420,110,140);
+        drawRobot(270,460,260,95);
+        drawRobot(420,310,80,10);
+        drawRobot(570,390,180,40);
 
 
+    }
+
+    void drawRobot(int x, int y, int bodyHeight, int neckHeight) {
+
+        int radius = 45;
+        int ny = y - bodyHeight - neckHeight - radius;
+
+
+        strokeWeight(2);
+        ellipseMode(RADIUS);
+
+        //neck
+
+        stroke(102);
+        line(x+2,y-bodyHeight,x+2,ny);
+        line(x+12,y-bodyHeight,x+12,ny);
+        line(x+22,y-bodyHeight,x+22,ny);
+
+        //antenna
+        line(x+12,ny,x-18,ny-43);
+        line(x+12,ny,x+42,ny-99);
+        line(x+12,ny,x+78,ny+15);
+
+
+        //body
+        noStroke();
+        fill(102);
+        ellipse(x,y-33,33,33);
+        fill(0);
+        rect(x-45,y-bodyHeight,90,bodyHeight-33);
+        fill(102);
+        rect(x-45,y-bodyHeight+17,90,6);
+
+        //head
+        fill(0);
+        ellipse(x+12,ny,radius,radius);
+        fill(255);
+        ellipse(x+24,ny-6,14,14);
+        fill(0);
+        ellipse(x+24,ny-6,3,3);
+        fill(153);
+        ellipse(x,ny-8,5,5);
+        ellipse(x+30,ny-26,4,4);
+        ellipse(x+41,ny+6,3,3);
+    }
+
+    float calculateMars (float w) {
+        float newWeight = w * 0.38f;
+        return newWeight;
     }
 
     void rollDice(int numSides) {
