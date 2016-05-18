a3e2cf0495f54a722ef6bae9547aa2b47150dae2
Freshield <zxdsw199182@gmail.com>
2016-05-18 15:49:21 -0400

    test array

--- G_Started_W_Processing/src/Chp10.java
+++ G_Started_W_Processing/src/Chp10.java
@@ -11,9 +11,17 @@ public class Chp10 extends PApplet {
         PApplet.main("Chp10");
     }
 
-    float x1 = -20;
-    float x2 = 20;
-    float[] x = new float[3000];
+//    float x1 = -20;
+//    float x2 = 20;
+//    float[] x = new float[3000];
+//    float[] x = {-20, 20};
+//    float[] gray;
+
+//    int num = 60;
+//    int x[] = new int[num];
+//    int y[] = new int[num];
+
+//    JitterBug[] bugs = new JitterBug[50];
 
 
     public void settings() {
@@ -24,22 +32,96 @@ public class Chp10 extends PApplet {
     }
 
     public void setup() {
-        noStroke();
-        fill(255,200);
-        for (int i = 0;i < x.length; i++) {
-            x[i] = random(-300,500);
-        }
+//        noStroke();
+//        fill(255,200);
+//        for (int i = 0;i < x.length; i++) {
+//            x[i] = random(-300,500);
+//        }
+
+//        x = new int[2];
+//        x[0] = 12;
+//        x[1] = 2;
+
+//        noStroke();
+//        println(x.length);
+//        int[] y = new int[1972];
+//        println(y.length);
+
+//        gray = new float[width];
+//        for (int i = 0; i < gray.length; i++) {
+//            gray[i] = ((int)random(0,2))*255;
+//        }
+
+//        noStroke();
+
+//        for (int i = 0; i < bugs.length; i++) {
+//            float x = random(width);
+//            float y = random(height);
+//            int r = i + 2;
+//            bugs[i] = new JitterBug(x,y,r);
+//        }
     }
 
     public void draw() {
 
-        background(0);
-        for (int i = 0;i < x.length; i++) {
-            x[i] += 0.5;
-            float y = i * 0.4f;
-            arc(x[i],y,12,12,0.52f,5.76f);
+//        background(0);
+//        for (int i = 0;i < x.length; i++) {
+//            x[i] += 0.5;
+//            float y = i * 0.4f;
+//            arc(x[i],y,12,12,0.52f,5.76f);
+//        }
+
+//        background(0);
+//        x[0] += 0.5;
+//        x[1] += 0.5;
+//        arc(x[0],30,40,40,0.52f,5.76f);
+//        arc(x[1],30,40,40,0.52f,5.76f);
+
+//        for (int i = 0; i < gray.length; i++) {
+//            stroke(gray[i]);
+//            line(i,0,i,height);
+//        }
+
+//        background(0);
+//        for (int i = x.length-1; i > 0; i--) {
+//            x[i] = x[i - 1];
+//            y[i] = y[i - 1];
+//        }
+//        x[0] = mouseX;
+//        y[0] = mouseY;
+//        for (int i = 0; i < x.length; i++) {
+//            fill(i * 4);
+//            ellipse(x[i],y[i],40,40);
+//        }
+
+        for (int i = 0; i < bugs.length; i++) {
+            bugs[i].move();
+            bugs[i].display();
         }
 
+
+    }
+
+    public class JitterBug {
+        float x;
+        float y;
+        int diameter;
+        float speed = 2.5f;
+
+        JitterBug(float tempX, float tempY,int tempDiameter) {
+            x = tempX;
+            y = tempY;
+            diameter = tempDiameter;
+        }
+
+        void move(){
+            x += random(-speed,speed);
+            y += random(-speed,speed);
+        }
+
+        void display(){
+            ellipse(x,y,diameter,diameter);
+        }
     }
 
 }
